1a2279c78e837788d72541e489cb84c6526bcfe8
Glitch (hello-express) <none>
2019-08-13 20:13:33 +0000

    💡🍡 Checkpoint ./package.json:6094142/53 ./server.js:6094142/1862

--- server.js
+++ server.js
@@ -4,17 +4,42 @@
 // init project
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken'); 
+const uuidv1 = require('uuid/v1');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.post('/', function (req, res) {
+  
+  console.log(req);
+  
+var uuid = uuidv1();
+var payload = {
+  name: 'Phone number guy',
+  email: 'phonenumberguy@example.com',
+  phone: '6085555454',
+  iat: Math.floor(Date.now() / 1000),
+  jti: uuid,
+  external_id: 'slughorn4'
+};
+
+var token = jwt.sign(payload, process.env.SECRET);
+  
+  
+  res.send(JSON.stringify({ "jwt": token}));
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
