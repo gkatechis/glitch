d50c76a5e9119cf40698e4dea5314ca5cb4e4df8
Glitch (hello-express) <none>
2020-02-12 21:55:45 +0000

    💠🍴 Checkpoint ./server.js:11313791/2345

--- server.js
+++ server.js
@@ -2,45 +2,45 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken'); 
-const uuidv1 = require('uuid/v1');
+const jwt = require("jsonwebtoken");
+const uuidv1 = require("uuid/v1");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.post('/', function (req, res) {
-  
+app.post("/", function(req, res) {
   console.log(req);
-  
-var uuid = uuidv1();
-var payload = {
-  name: 'SDK JWT new guy ',
-  email: 'test@test.testing',
-  phone: '6085555454',
-  iat: Math.floor(Date.now() / 1000),
-  jti: uuid
-};
 
-var token = jwt.sign(payload, process.env.SECRET);
-  
-  
-  res.send(JSON.stringify({ "jwt": token}));
-})
+  var uuid = uuidv1();
+  var payload = {
+    name: "SDK JWT new guy ",
+    email: "test@test.testing",
+    phone: "6085555454",
+    iat: Math.floor(Date.now() / 1000),
+    jti: uuid
+  };
+
+  var token = jwt.sign(payload, process.env.SECRET);
+
+  res.send(JSON.stringify({ jwt: token }));
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
